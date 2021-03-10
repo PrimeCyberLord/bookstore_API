9e10df9dc70c629bafa54cf3011587c39153482d
Brian Matlou <thatobrianmatlou@gmail.com>
2021-03-10 09:56:25 +0200

    Project Structure

---
+++ src/main/java/com/psybergate/absaassessment/AbsaAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.psybergate.absaassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsaAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsaAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psybergate/absaassessment/AbsaAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psybergate.absaassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsaAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
