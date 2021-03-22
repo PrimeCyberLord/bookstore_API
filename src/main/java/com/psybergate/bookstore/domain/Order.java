a3ad1dcf7871366d569c907fbfa41f3b02cc5a4b
Brian Matlou <thatobrianmatlou@gmail.com>
2021-03-22 19:47:40 +0200

    added tests

--- src/main/java/com/psybergate/bookstore/domain/Order.java
+++ src/main/java/com/psybergate/bookstore/domain/Order.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -17,7 +18,7 @@ public class Order extends BaseEntity {
     @ManyToOne
     private User owner;
 
-    @OneToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     private List<Book> books;
 
     private double totalPrice;
@@ -26,21 +27,35 @@ public class Order extends BaseEntity {
 
     private LocalDate dateCreated;
 
-    public void init(){
-         totalPrice = calcTotalPrice(books);
-         dateCreated = LocalDate.now();
+    public void init() {
+        totalPrice = calcTotalPrice(books);
+        dateCreated = LocalDate.now();
     }
 
     private double calcTotalPrice(List<Book> books) {
 
         double total = 0;
 
-        for(Book book:books){
-            total+=book.getPrice()*book.getQuantity();
+        for (Book book : books) {
+            total += book.getPrice() * book.getQuantity();
         }
 
         return total;
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (!(this.getClass() == o.getClass())) return false;
+        Order order = (Order) o;
+        if (!(this.getId() == order.getId())) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId());
     }
 
 }
